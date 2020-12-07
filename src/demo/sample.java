0a8134cc231790594b73dd0564f0b5b9fe8a1f6a
jeevan <jeevan@LAPTOP-8VC9E1EC>
2020-12-07 18:25:05 +0530

    [jeevan]:jhbcsbc

--- src/demo/sample.java
+++ src/demo/sample.java
@@ -6,7 +6,7 @@ public static void main(String[] args) {
 	System.out.println("good morning");
 	System.out.println("velocity class");
 	System.out.println("bc");
-	
+	System.out.println("poonam");
 	
 	
 	
